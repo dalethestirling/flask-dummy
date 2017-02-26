99379b3b3178413bb42e1a4df3e27c9ea2d09f9d
Dale Stirling <dstirling@deloitte.com.au>
2017-02-26 17:42:53 +1100

    fixed bugs in push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,15 @@
-/bin/bash
+#! /bin/bash
 
 oc login -u system:admin
 REGISTRY_IP=$(oc get svc docker-registry -n default -o template --template={{.spec.clusterIP}})	
 
+echo "Have registry IP: ${REGISTRY_IP}"
+
 oc login -u developer
 OPENSHIFT_TOKEN=$(oc whoami -t)
 
-docker login -u developer -p ${OPENSHIFT_TOKEN} ${REGISTY_IP}:5000
+echo "Have token: ${OPENSHIFT_TOKEN}"
+
+docker login -u developer -p ${OPENSHIFT_TOKEN} ${REGISTRY_IP}:5000
 docker tag flask-demo:latest ${REGISTRY_IP}:5000/myproject/flask-demo
-docker push ${REGISTRY_IP}:5000/myproject/flask-demo
+docker push ${REGISTRY_IP}:5000/myproject/flask-demo:latest
