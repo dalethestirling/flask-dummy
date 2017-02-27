1e3390c32c46b59da583f06a1c16d8bd07f141fe
Dale Stirling <dstirling@deloitte.com.au>
2017-02-27 23:10:10 +1100

    updated and completed unit test

--- test/unit.py
+++ test/unit.py
@@ -1,5 +1,16 @@
-import urllib3
+#!/usr/bin/env python
 
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://127.0.0.1:5000/hello')
-assert r.data == 'world'
+import unittest
+import urllib2
+import sys
+
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        result = urllib2.urlopen('http://%s/hello' % (testing_endpoint)).read()        
+        self.assertEqual(result, 'world')
+
+if __name__ == '__main__':
+    testing_endpoint = str(sys.argv[1])
+    print testing_endpoint
+    del sys.argv[1:]
+    unittest.main()
